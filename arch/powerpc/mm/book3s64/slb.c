32ce6878001925c206b4a369b90d12675c0a919a
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2025-08-23 22:01:00 +0530

    powerpc/book3s64/slb: Add slb faults to vmstat
    There were good optimization written in past which reduces the number of
    slb faults e.g. during context switches [1]. However if one wants to
    measure total number of slb faults then there is no easy way of
    measuring one e.g. number of slb faults during bootup.
    
    This adds slb faults as part of vmstat counter to easily measure total
    number of slb faults for book3s64.
    
    Note: slb fault handling is defined as raw interrupt handler which says:
     * raw interrupt handlers must not enable or disable interrupts, or
     * schedule, tracing and instrumentation (ftrace, lockdep, etc) would
     * not be advisable either, although may be possible in a pinch, the
     * trace will look odd at least.
    
    Hence adding a vmstat counter looks a plausible and safe option, to at-
    least measure the number of slb kernel & user faults in the system.
    
    [1]: https://lore.kernel.org/all/20181013131836.26764-4-mpe@ellerman.id.au/
    
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>

--- arch/powerpc/mm/book3s64/slb.c
+++ arch/powerpc/mm/book3s64/slb.c
@@ -22,6 +22,7 @@
 #include <linux/context_tracking.h>
 #include <linux/mm_types.h>
 #include <linux/pgtable.h>
+#include <linux/vmstat.h>
 
 #include <asm/udbg.h>
 #include <asm/code-patching.h>
@@ -780,6 +781,7 @@ DEFINE_INTERRUPT_HANDLER_RAW(do_slb_fault)
 #ifdef CONFIG_DEBUG_VM
 		local_paca->in_kernel_slb_handler = 0;
 #endif
+		count_vm_event(SLB_KERNEL_FAULTS);
 		return err;
 	} else {
 		struct mm_struct *mm = current->mm;
@@ -792,6 +794,7 @@ DEFINE_INTERRUPT_HANDLER_RAW(do_slb_fault)
 		if (!err)
 			preload_add(current_thread_info(), ea);
 
+		count_vm_event(SLB_USER_FAULTS);
 		return err;
 	}
 }
