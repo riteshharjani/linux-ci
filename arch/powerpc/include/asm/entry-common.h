8b58c8315cdaf5ed8b8e7fa62a91ea2f4440154f
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2025-11-02 17:23:52 +0530

    powerpc: Prepare to build with generic entry/exit framework
    This patch introduces preparatory changes needed to support building
    PowerPC with the generic entry/exit (irqentry) framework.
    
    The following infrastructure updates are added:
     - Add a syscall_work field to struct thread_info to hold SYSCALL_WORK_* flags.
     - Provide a stub implementation of arch_syscall_is_vdso_sigreturn(),
       returning false for now.
     - Introduce on_thread_stack() helper to detect if the current stack pointer
       lies within the task’s kernel stack.
    
    These additions enable later integration with the generic entry/exit
    infrastructure while keeping existing PowerPC behavior unchanged.
    
    No functional change is intended in this patch.
    
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

---
+++ arch/powerpc/include/asm/entry-common.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _ASM_PPC_ENTRY_COMMON_H
+#define _ASM_PPC_ENTRY_COMMON_H
+
+#ifdef CONFIG_GENERIC_IRQ_ENTRY
+
+#include <asm/stacktrace.h>
+
+#endif /* CONFIG_GENERIC_IRQ_ENTRY */
+#endif /* _ASM_PPC_ENTRY_COMMON_H */
--- arch/powerpc/include/asm/stacktrace.h
+++ arch/powerpc/include/asm/stacktrace.h
@@ -10,4 +10,10 @@
 
 void show_user_instructions(struct pt_regs *regs);
 
+static inline bool on_thread_stack(void)
+{
+	return !(((unsigned long)(current->stack) ^ current_stack_pointer)
+			& ~(THREAD_SIZE - 1));
+}
+
 #endif /* _ASM_POWERPC_STACKTRACE_H */
